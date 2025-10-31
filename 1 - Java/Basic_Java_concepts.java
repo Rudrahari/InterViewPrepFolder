786dc7130a0afb2852066a397d6ce2ac23192117
Rajesh2723 <hemannaparvathig@gmail.com>
2025-10-31 14:13:42 +0530

    added changes for string methods

--- 1 - Java/Basic_Java_concepts.java
+++ 1 - Java/Basic_Java_concepts.java
@@ -11,15 +11,33 @@ public class Basic_Java_concepts {
         }
     }
 
+    public static void stringMethodsBuilder(){
+        StringBuilder sb= new StringBuilder("Fast");
+        sb.append(" Builder");
+        System.out.println(sb.toString());
+    }
+
+    public static void stringBufferMethod(){
+        StringBuffer sb=new StringBuffer("Hello");
+        sb.append(" Java");
+        sb.insert(0,"Welcome");
+        sb.reverse();
+        System.out.print(sb.toString());
+    }
+
     public static void main(String [] args){
 
         Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
         int n=scanner.nextInt();
         int []arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
         print(arr,n);
+        stringMethodsBuilder();//Not a Thread safe
+        stringBufferMethod(); //Thread safe
+
 
     }
 }
